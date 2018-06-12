2f1182d60f9a39bf6da6f83837bdb7e4320b903e
Slavik89 <terakrokos@gmail.com>
2018-06-12 18:19:45 +0300

    Changes #17 were added

--- script_02.js
+++ script_02.js
@@ -1,4 +1,6 @@
 function showMenu() {
     var y = document.getElementById("switcher");
+    var z = document.getElementById("desappearButton");
     y.style.display = "block";      
+    z.style.display = "none";
 }
