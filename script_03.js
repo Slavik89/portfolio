ef14ade566bde657b96fbad7fd36c3e02cbab51f
Slavik <terakrokos@gmail.com>
2018-06-13 22:40:17 +0300

    Changes #20 were added

--- script_03.js
+++ script_03.js
@@ -1,9 +1,9 @@
-function showMenu() {
+function closeMenu() {
     var y = document.querySelector('#nav ul');    
     var el = document.querySelector('#nav button');    
     var cross = document.querySelector('#nav img');
     
-    y.style.display = 'block';      
-    el.style.display = 'none';
-    cross.style.display = 'block';
+    y.style.display = 'none';      
+    el.style.display = 'block';
+    cross.style.display = 'none';
 }
